df599394094a567a6088e3d2dd25ec44d82a6d06
cocom33 <ancintcococ3@gmail.com>
2022-03-19 09:50:39 +0700

    testes

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+
+            $table->date('tanggal_lahir');
+            $table->string('pekerjaan')->nullable();
+            $table->string('kota')->nullable();
+            $table->text('bio_profil')->nullable();
+            $table->string('gambar_profil')->default('defaul_profile.jpg');
+            $table->tinyInteger('background_profil')->default(1);
+
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
